ae00b3b16e5127b3086aea1231b63fa36706ba35
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-11 04:50:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+  
+  const newdrivers=array.map(function(num){
+    return num.toLowerCase();
+  });
+  return newdrivers;
+}
+
+function nameToAttributes(array){
+  const newdrivers=array.map(function(num){
+    const first=num.split(" ")[0];
+    const last=num.split(" ")[1];
+    return {firstName: first, lastName: last};
+  });
+  return newdrivers;
+}
+
+function attributesToPhrase(array){
+  
+}
